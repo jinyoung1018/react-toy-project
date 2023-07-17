ffcf53a19e4c88690f65d3b49666cf966d24be91
jinyoung1018 <jywsb1816@gmail.com>
2023-07-17 17:36:10 +0900

    contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,6 +3,27 @@ const ContactForm = () => {
         <div className="ContactForm">
             <h1> Drop us a line</h1>
             <div>어쩌구저쩌구 어쩌구저쩌구 연락 주세요.</div>
+            <div className="input_area">
+                <div className="inputbox">
+                    <input type="text" ></input>
+                    <label>Name</label>
+                    <span></span>     
+                </div>
+                <div className="inputbox">
+                    <input type="text" ></input>
+                    <label>Email</label>
+                    <span></span>     
+                </div>
+                <div className="inputbox">
+                    <input type="text" ></input>
+                    <label>Message</label>
+                    <span></span>     
+                </div>
+                <button className="btn">Send Message</button>
+          
+                
+                
+            </div>
         </div>
     )
 }
